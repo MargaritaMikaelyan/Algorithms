5ab77d51790cba27055e39c2a60b7be59b8f26ba
MargaritaMikaelyan <56021259+MargaritaMikaelyan@users.noreply.github.com>
2020-07-05 19:09:03 +0400

    number theory / problems from timus

---
+++ Algorithms/NumberTheory/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberTheory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static void Timus1049()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var n = int.Parse(Console.ReadLine());
+                DivCount(n);
+            }
+
+            long ans = 1;
+            for (int i = 0; i < 10000; i++)
+            {
+                ans *= divisors[i] + 1;
+                ans %= 10;
+            }
+
+            Console.WriteLine(ans);
+        }
+
+        static int[] divisors = new int[10000];
+        static void DivCount(int n)
+        {
+            for (int i = 2; i * i <= n; i++)
+            {
+                while (n % i == 0)
+                {
+                    divisors[i]++;
+                    n /= i;
+                }
+            }
+            if (n != 1)
+                divisors[n]++;
+        }
+
+        static void Timus2047()
+        {
+            var sum = 1568617;
+            var divisors = new int[1568618];
+            var ans = new int[100005];
+
+            // Calculates the number of divisors
+            for (var i = 1; i <= sum; i++) 
+                for (var j = i; j <= sum; j += i)
+                    divisors[j]++;
+
+            for (int i = 100000; i >= 0; i--)
+            {
+                ans[i] = divisors[sum];
+                sum -= ans[i];
+            }
+
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(ans[i] + " ");
+            }
+        }
+
+        static void Timus1356()
+        {
+            var t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine());
+                if (IsPrime(n))
+                    Console.WriteLine(n);
+                else
+                {
+                    if (n % 2 == 0)
+                    {
+                        for (int j = 2; j < n; j++)
+                        {
+                            if (IsPrime(n - j) && IsPrime(j))
+                            {
+                                Console.WriteLine(j + " " + (n - j));
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (IsPrime(n - 2))
+                        {
+                            Console.WriteLine(2 + " " + (n - 2));
+                        }
+                        else
+                        {
+                            var k = n - 3;
+                            for (int j = 2; j < n; j++)
+                            {
+                                if (IsPrime(k - j) && IsPrime(j))
+                                {
+                                    Console.WriteLine("3 " + (k - j) + " " + j);
+                                    break;
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+        static bool IsPrime(int n)
+        {
+            if (n == 1)
+                return false;
+            for (var i = 2; i * i <= n; i++)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+
+        static void Timus1740()
+        {
+            var input = Console.ReadLine().Split();
+            var l = int.Parse(input[0]);
+            var k = int.Parse(input[1]);
+            var h = int.Parse(input[2]);
+
+            if (l % k == 0)
+            {
+                Console.WriteLine($"{l / k * h:0.00000000000}" + " " + $"{l / k * h:0.00000000000}");
+            }
+            else
+            {
+                Console.WriteLine($"{l / k * h:0.00000000000}" + " " + $"{(l / k + 1) * h:0.00000000000}");
+            }
+        }
+
+        static void Timus1044()
+        {
+            N = int.Parse(Console.ReadLine());
+            GetChoice(0, 0);
+
+            int ans = 0;
+            for (int i = 0; i <= 36; ++i)
+                ans += count[i] * count[i];
+            Console.WriteLine(ans);
+        }
+        private static int N;
+        private static int[] count = new int[37];
+        static void GetChoice(int n, int sum)
+        {
+            if (n == N / 2)
+                ++count[sum];
+            else
+            {
+                for (int i = 0; i < 10; ++i)
+                    GetChoice(n + 1, sum + i);
+            }
+        }
+
+        static void Timus1940()
+        {
+            var input = Console.ReadLine().Split();
+            var A = int.Parse(input[0]);
+            var B = int.Parse(input[1]);
+            var k = int.Parse(input[2]);
+            Sieve(k + 1);
+            var ans = B - Query(A, A + B, primes.Count - 1);
+            foreach (var p in primes)
+            {
+                if (p > A && p <= A + B)
+                    ans++;
+            }
+
+            Console.WriteLine(ans);
+        }
+        static int Query(int a, int b, int k)
+        {
+            if (b <= a || k == -1)
+                return 0;
+            return b / primes[k] - a / primes[k] +
+                Query(a, b, k - 1) - Query(a / primes[k], b / primes[k], k - 1);
+        }
+
+        static List<int> primes;
+        static void Sieve(int max)
+        {
+            var is_prime = new bool[305];
+            primes = new List<int>();
+            for (int p = 2; p <= max; p++)
+            {
+                if (is_prime[p] == false)
+                {
+                    primes.Add(p);
+                    for (int i = p * 2; i <= max; i += p)
+                        is_prime[i] = true;
+                }
+            }
+        }
+
+    }
+}
