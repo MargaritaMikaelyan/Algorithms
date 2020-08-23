540e3beb8e2cf8753eb0770d29d85017c8c94e3e
MargaritaMikaelyan <56021259+MargaritaMikaelyan@users.noreply.github.com>
2020-08-23 14:06:50 +0400

    Timus 1993

--- Algorithms/Timus/Program.cs
+++ Algorithms/Timus/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Globalization;
 using System.Linq;
 using System.Numerics;
-using System.Runtime.ExceptionServices;
 using System.Text;
 
 namespace Timus
