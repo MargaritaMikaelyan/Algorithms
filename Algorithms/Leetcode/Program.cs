a0d1a92e49079a65fb76df397f17638b70363bc3
Maga Mikaelyan <margarita.mikaelyan@picsart.com>
2021-05-16 19:22:24 +0400

    leetcode problems

---
+++ Algorithms/Leetcode/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var n1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
+            //var n2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+            //Console.WriteLine(AddTwoNumbers(n1, n2));
+            //Console.WriteLine(GetSum(-16, -115));
+            //LetterCasePermutation("a1b2");
+        }
+
+        // "a1b24c"
+        // "A1b24c"
+        // "A1B24c"
+        // "A1b24C" ...
+        public static IList<string> LetterCasePermutation(string s)
+        {
+            Permutation(s);
+            return res;
+        }
+
+        private static List<string> res = new List<string>();
+        static void Permutation(string s, int i = 0, string temp = "")
+        {
+            if (i == s.Length)
+            {
+                res.Add(temp);
+                return;
+            }
+
+            Permutation(s, i + 1, temp + s[i]);
+            if (char.IsLower(s[i]))
+                Permutation(s, i + 1, temp + char.ToUpper(s[i]));
+            else if (char.IsUpper(s[i]))
+                Permutation(s, i + 1, temp + char.ToLower(s[i]));
+        }
+
+        public static int GetSum(int a, int b)
+        {
+            return b == 0 ? a : GetSum(a ^ b, (a & b) * 2);
+        }
+
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var current = new ListNode();
+            var res = current;
+            var rem = 0;
+            while (l1 != null || l2 != null)
+            {
+                var n1 = l1?.val ?? 0;
+                var n2 = l2?.val ?? 0;
+                var sum = n1 + n2 + rem;
+                rem = sum / 10;
+                current.val = sum % 10;
+                if (l1?.next == null && l2?.next == null)
+                    break;
+                current.next = new ListNode();
+                current = current.next;
+
+                l1 = l1?.next;
+                l2 = l2?.next;
+            }
+
+            if (rem == 1)
+                current.next = new ListNode(1);
+            return res;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
